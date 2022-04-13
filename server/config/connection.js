19757306d0f755db8fbfdaff46b10ad7b20bd498
“jameschukwu“ <okoriechukwu@yahoo.com>
2022-04-13 12:01:12 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
-
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+  //useNewUrlParser: true,
+ // useUnifiedTopology: true,
+ // useCreateIndex: true,
+ // useFindAndModify: false,
+//});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/mighty-wave-68152',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
 module.exports = mongoose.connection;
