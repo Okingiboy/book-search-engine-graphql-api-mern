6448efdc57b07d76f8caf958974ce9c2792cf786
“jameschukwu“ <okoriechukwu@yahoo.com>
2022-04-13 14:49:42 -0500

    infinite-bayou-40381

--- server/config/connection.js
+++ server/config/connection.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
  // useFindAndModify: false,
 //});
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/mighty-wave-68152',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/infinite-bayou-40381',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
