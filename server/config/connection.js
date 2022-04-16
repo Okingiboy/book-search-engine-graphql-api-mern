a860085133f0da76905d60ede3215d5489ee926b
“jameschukwu“ <okoriechukwu@yahoo.com>
2022-04-16 11:52:48 -0500

    updated files

--- server/config/connection.js
+++ server/config/connection.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
  // useFindAndModify: false,
 //});
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/infinite-bayou-40381',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/mysterious-falls-78296',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
